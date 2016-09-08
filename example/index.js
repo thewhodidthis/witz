1f007748381ac70be44c87bb89ee2e5b93f3c7d6
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-09-08 19:25:08 +0200

    shuffle things around a little bit

--- example/index.js
+++ example/index.js
@@ -8,33 +8,23 @@ var target = document.getElementById('canvas').getContext('2d');
 // Yes the id attribute is the same as that of master but buffer is orphan at this point
 var buffer = target.canvas.cloneNode();
 
-var blob = new Blob([document.getElementById('worker').textContent]);
-var blobURL = window.URL.createObjectURL(blob);
+var workerBlob = new Blob([document.getElementById('worker').textContent]);
+var workerBlobUrl = (window.URL || window.webkitURL).createObjectURL(workerBlob);
 
-var worker = new Worker(blobURL);
+var worker = new Worker(workerBlobUrl);
 
 html.className = 'html';
 
 // Apply different styles when loaded inside of an iframe
 if (window !== window.top) {
-  html.classList.add('is-iframe');
+  html.className += ' is-iframe';
 }
 
 worker.addEventListener('message', function _onDoneProcessing(e) {
-  output.src = e.data.result;
+  output.setAttribute('src', e.data.result);
 }, false);
 
-source.src = '/sprite.png';
-
-source.addEventListener('load', function _onOriginalLoaded(e) {
-  buffer.getContext('2d').drawImage(source, 0, 0);
-
-  worker.postMessage({
-    input: buffer.toDataURL('image/jpeg', 0.01)
-  });
-}, false);
-
-output.addEventListener('load', function _onFilteredLoaded(e) {
+output.addEventListener('load', function _onOutputLoaded(e) {
   target.drawImage(output, 0, 0);
 
   // DANGER! DANGER!
@@ -42,3 +32,13 @@ output.addEventListener('load', function _onFilteredLoaded(e) {
   //   input: target.canvas.toDataURL('image/jpeg', 0.01)
   // });
 }, false);
+
+source.addEventListener('load', function _onInputLoaded(e) {
+  buffer.getContext('2d').drawImage(source, 0, 0);
+
+  worker.postMessage({
+    input: buffer.toDataURL('image/jpeg', 0.01)
+  });
+}, false);
+
+source.setAttribute('src', '/sprite.png');
