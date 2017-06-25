61be9f7b2ddd68cc3c3629e5422c7c6081978f83
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-26 01:58:14 +0200

    test: check for data in/out

--- test/index.js
+++ test/index.js
@@ -1,8 +1,15 @@
 const test = require('tape');
-const filter = require('../');
+const createFilter = require('../');
 
-test('will return', (t) => {
+test('will operate', (t) => {
+  const canvas = Object.assign(document.createElement('canvas'), { width: 10, height: 10 });
+  const source = canvas.toDataURL('image/jpeg', 0.01);
+
+  const filter = createFilter();
+  const result = filter(source);
+
   t.equal(typeof filter, 'function', 'returns funtion on init');
+  t.ok(result.indexOf('image/jpeg'), 'output type is a match');
   t.end();
 });
 
