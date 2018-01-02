638d3b50083d679a3fc27fff51f80e365692ec6d
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-01-02 11:16:21 +0100

    example: cleanup main script, retry on image loading error

--- example/index.js
+++ example/index.js
@@ -8,54 +8,67 @@ if (window !== window.top) {
   document.documentElement.classList.add('is-iframe');
 }
 
+var halfPI = Math.PI * 0.5;
 var params = [
-  { count: 0 },
+  { depth: 0 },
   { up: true },
-  { count: 9 }
+  { depth: 9 }
 ];
 
-Array.from(images).map(function (img) { return img.alt; }).forEach(function (file, i) {
+Array.from(images).map(function (img) { return img.alt; }).forEach(function (src, i) {
   var config = params[i % params.length];
-
   var canvas = boards[i];
-  var master = canvas.getContext('2d');
+
+  var target = canvas.getContext('2d');
   var buffer = canvas.cloneNode().getContext('2d');
 
-  var source = document.createElement('img');
+  var master = document.createElement('img');
   var output = document.createElement('img');
+
   var worker = new Worker('worker.js');
 
+  var update = function () {
+    // Works best with JPG images
+    var source = buffer.canvas.toDataURL('image/jpeg', 0.5);
+
+    worker.postMessage({ config: config, source: source });
+  };
+
+  // This needs fixing, fires more times than expected
   worker.addEventListener('message', function (e) {
     output.setAttribute('src', e.data.result);
   });
 
   output.addEventListener('load', function () {
-    master.save();
+    target.save();
 
     // Only rotate glitch versions
-    if (config.up && output.src.indexOf('data') >= 0) {
-      master.translate(0, canvas.height);
-      master.rotate(-Math.PI * 0.5);
+    if (config.up) {
+      target.translate(0, canvas.height);
+      target.rotate(-halfPI);
     }
 
-    master.drawImage(output, 0, 0);
-    master.restore();
+    target.drawImage(output, 0, 0);
+    target.restore();
   });
 
-  // Because iOS may struggle at times,
-  // avoid drawing blanks this way
-  output.setAttribute('src', file);
-
-  source.addEventListener('load', function () {
-    var angle = config.up ? Math.PI * 0.5 : 0;
-    var shift = config.up ? -1 * canvas.height : 0;
-
-    buffer.rotate(angle);
-    buffer.drawImage(source, 0, shift);
-    worker.postMessage({ config: config, source: buffer.canvas.toDataURL('image/jpeg', 0.5) });
+  // Avoid drawing blanks
+  output.addEventListener('error', function () {
+    // Try again
+    update();
   });
 
-  source.setAttribute('src', file);
+  master.addEventListener('load', function () {
+    if (config.up) {
+      buffer.rotate(halfPI);
+      buffer.translate(0, -canvas.height);
+    }
+
+    buffer.drawImage(master, 0, 0);
+    update();
+  });
+
+  master.setAttribute('src', src);
 });
 
 }());
