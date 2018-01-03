37bff019835328d99d893751ebe85b3614478198
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-01-03 13:37:32 +0100

    test: have eslint ignore max-len (data-url)

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const { ok, equal } = require('tapeless')
 const createFilter = require('./')
 
+/* eslint max-len: 0 */
 const sample = 'data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII='
 
 const filter = createFilter()
