25c31fea93e7612be598411fe5a1fa881043237d
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-21 06:49:11 +0100

    module: buble not the cjs version

--- index.js
+++ index.js
@@ -5,21 +5,17 @@
 
 // Corrupt by recursively inserting random pick from base64
 // charset at random index along target string
-var glitch = function (lookup) {
-  var random = function (peak) {
-    if ( peak === void 0 ) peak = lookup.length;
-
-    return Math.floor(Math.random() * peak);
-  };
-  var filter = function (data, turn) {
+const glitch = (lookup) => {
+  const random = (peak = lookup.length) => Math.floor(Math.random() * peak);
+  const filter = (data, turn) => {
     if (!turn) {
       return data
     }
 
-    var mark = random(data.length);
-    var seed = lookup.charAt(random());
+    const mark = random(data.length);
+    const seed = lookup.charAt(random());
 
-    var crop = data.substring(0, mark) + seed + data.substring(mark + 1);
+    const crop = data.substring(0, mark) + seed + data.substring(mark + 1);
 
     return filter(crop, turn - 1)
   };
@@ -27,9 +23,9 @@ var glitch = function (lookup) {
   return filter
 };
 
-var witz = function (options) {
+const witz = (options) => {
   // Merge options and defaults
-  var ref = Object.assign({
+  const { chars, depth } = Object.assign({
     // List of characters to choose from
     // Source: https://tools.ietf.org/html/rfc4648#page-5
     chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
@@ -37,26 +33,21 @@ var witz = function (options) {
     // Resolution, mutations per call
     depth: 23
   }, options);
-  var chars = ref.chars;
-  var depth = ref.depth;
 
   // Strap in
-  var filter = glitch(chars);
+  const filter = glitch(chars);
 
   // Expects and returns dataURL or plain base64 encoded string,
   // allows for overriding depth
-  return function (target, N) {
-    if ( target === void 0 ) target = '';
-    if ( N === void 0 ) N = depth;
-
+  return (target = '', N = depth) => {
     // Bypass
     if (!N) {
       return target
     }
 
     // Extract data past the comma in dataURL if need be
-    var split = target.indexOf(',') + 1;
-    var scoop = target.slice(split);
+    const split = target.indexOf(',') + 1;
+    const scoop = target.slice(split);
 
     // Stitch up
     return target.slice(0, split) + filter(scoop, N)
@@ -64,4 +55,3 @@ var witz = function (options) {
 };
 
 module.exports = witz;
-
