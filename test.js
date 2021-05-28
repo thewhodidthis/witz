5592bc6a5a212256c7a5f665cbcc09ec5e0231da
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-05-28 19:42:26 +0100

    test: use imports

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-'use strict'
-
-const { ok, equal } = require('tapeless')
-const createFilter = require('./')
+import assert from 'tapeless'
+import createFilter from './main.js'
 
 /* eslint max-len: 0 */
 const sample = 'data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII='
 
+const { ok, equal } = assert
 const filter = createFilter()
 const result = filter(sample)
 
