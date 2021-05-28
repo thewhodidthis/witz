d0887dd0def38aa90d4f12299f48cf0eeb166e68
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-05-28 19:46:18 +0100

    example: rebuild

--- example/witz.js
+++ example/witz.js
@@ -1,63 +1,43 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.witz = factory());
-}(this, (function () { 'use strict';
+var witz = (() => {
+  var __defProp = Object.defineProperty;
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
 
-  // # Witz
-  // Helps produce glitch
-
-  // Corrupt by recursively inserting random pick from base64
-  // charset at random index along target string
-  const glitch = (lookup) => {
+  // main.js
+  var main_exports = {};
+  __export(main_exports, {
+    default: () => main_default
+  });
+  var glitch = (lookup) => {
     const random = (peak = lookup.length) => Math.floor(Math.random() * peak);
     const filter = (data, turn) => {
       if (!turn) {
-        return data
+        return data;
       }
-
       const mark = random(data.length);
       const seed = lookup.charAt(random());
-
       const crop = data.substring(0, mark) + seed + data.substring(mark + 1);
-
-      return filter(crop, turn - 1)
+      return filter(crop, turn - 1);
     };
-
-    return filter
+    return filter;
   };
-
-  const witz = (options) => {
-    // Merge options and defaults
+  var witz = (options) => {
     const { chars, depth } = Object.assign({
-      // List of characters to choose from
-      // Source: https://tools.ietf.org/html/rfc4648#page-5
-      chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
-
-      // Resolution, mutations per call
+      chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
       depth: 23
     }, options);
-
-    // Strap in
     const filter = glitch(chars);
-
-    // Expects and returns dataURL or plain base64 encoded string,
-    // allows for overriding depth
-    return (target = '', N = depth) => {
-      // Bypass
+    return (target = "", N = depth) => {
       if (!N) {
-        return target
+        return target;
       }
-
-      // Extract data past the comma in dataURL if need be
-      const split = target.indexOf(',') + 1;
+      const split = target.indexOf(",") + 1;
       const scoop = target.slice(split);
-
-      // Stitch up
-      return target.slice(0, split) + filter(scoop, N)
-    }
+      return target.slice(0, split) + filter(scoop, N);
+    };
   };
-
-  return witz;
-
-})));
+  var main_default = witz;
+  return main_exports;
+})();
--- example/worker.js
+++ example/worker.js
@@ -1,7 +1,7 @@
 importScripts('witz.js')
 
 self.addEventListener('message', (e) => {
-  const filter = witz(e.data.config)
+  const filter = witz.default(e.data.config)
 
   self.postMessage({
     result: filter(e.data.source)
